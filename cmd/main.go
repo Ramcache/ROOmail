fe5adb6cffe8df21312655030a2bbf7ea5d8c03d
unknown <ramaro@internet.ru>
2024-11-29 14:19:35 +0300

    set TLS

--- cmd/main.go
+++ cmd/main.go
@@ -25,20 +25,32 @@ import (
 // @host localhost:8080
 // @BasePath /
 func main() {
+	// Загрузка .env файла
 	err := godotenv.Load()
 	if err != nil {
 		log.Printf("Error loading .env file: %v", err)
 	}
 
+	// Загрузка конфигурации
 	cfg := config.LoadConfig()
 
+	// Инициализация базы данных
 	database, err := db.InitDB(cfg.DatabaseURL)
 	if err != nil {
 		log.Fatalf("Failed to initialize database: %v", err)
 	}
 
+	// Инициализация маршрутизатора
 	r := router.NewRouter(database, cfg)
 
-	log.Printf("Server started at %s", cfg.ServerAddress)
-	log.Fatal(http.ListenAndServe(cfg.ServerAddress, r))
+	// Пути к сертификату и ключу
+	certFile := `C:\Users\Рамзан\server.crt`
+	keyFile := `C:\Users\Рамзан\server.key`
+
+	// Запуск HTTPS-сервера
+	log.Printf("Server started at https://%s", cfg.ServerAddress)
+	err = http.ListenAndServeTLS(cfg.ServerAddress, certFile, keyFile, r)
+	if err != nil {
+		log.Fatalf("Failed to start HTTPS server: %v", err)
+	}
 }
